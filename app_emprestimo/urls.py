f7137aa55f672e9e83761af9a6bdc8737083f20d
Lakh006 <heinen.leonardo06@gmail.com>
2025-04-01 22:25:28 -0300

    Deletar_colaborador

--- app_emprestimo/urls.py
+++ app_emprestimo/urls.py
@@ -4,4 +4,6 @@ from app_emprestimo import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('cadastrar/', views.cadastrar_colaborador, name='cadastrar'),
+    path('listar', views.listar_colaboradores, name='listar_colaboradores'),
+    path('deletar/<int:idCOLABORADOR>', views.deletar_colaborador, name='deletar_colaborador'),
 ]
--- app_emprestimo/views.py
+++ app_emprestimo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app_emprestimo.models import ColaboradorModel
 
 # Create your views here.
@@ -15,6 +15,13 @@ def cadastrar_colaborador(request):
     data_admissao = request.POST.get('data_admissao')
     colaborador = ColaboradorModel.objects.create(nome=nome, cpf=cpf, funcao=funcao, data_admissao=data_admissao)
 
+
+def deletar_colaborador(request, idCOLABORADOR):
+    colaborador = ColaboradorModel.objects.get(idCOLABORADOR=idCOLABORADOR)
+    colaborador.delete()
+    return redirect('listar_colaboradores')
+
+
+def listar_colaboradores(request):
     colaboradores = ColaboradorModel.objects.all()
     return render(request, 'app_emprestimo/pages/cadastrar.html', context={'colaboradores':colaboradores}) 
-
