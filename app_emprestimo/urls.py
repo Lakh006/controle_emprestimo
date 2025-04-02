6d2d1a6b362e16a792aaae96150abaefabbeee8f
Lakh006 <heinen.leonardo06@gmail.com>
2025-04-02 15:34:08 -0300

    Atualizar_colaborador

--- app_emprestimo/urls.py
+++ app_emprestimo/urls.py
@@ -4,6 +4,7 @@ from app_emprestimo import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('cadastrar/', views.cadastrar_colaborador, name='cadastrar'),
-    path('listar', views.listar_colaboradores, name='listar_colaboradores'),
+    path('listar/', views.listar_colaboradores, name='listar_colaboradores'),
+    path('atualizar/<int:idCOLABORADOR>',views.atualizar_colaborador, name='atualizar_colaborador'),
     path('deletar/<int:idCOLABORADOR>', views.deletar_colaborador, name='deletar_colaborador'),
 ]
--- app_emprestimo/views.py
+++ app_emprestimo/views.py
@@ -14,6 +14,7 @@ def cadastrar_colaborador(request):
     funcao = request.POST.get('funcao')
     data_admissao = request.POST.get('data_admissao')
     colaborador = ColaboradorModel.objects.create(nome=nome, cpf=cpf, funcao=funcao, data_admissao=data_admissao)
+    return render(request, 'app_emprestimo/pages/cadastrar.html')
 
 
 def deletar_colaborador(request, idCOLABORADOR):
@@ -24,4 +25,16 @@ def deletar_colaborador(request, idCOLABORADOR):
 
 def listar_colaboradores(request):
     colaboradores = ColaboradorModel.objects.all()
-    return render(request, 'app_emprestimo/pages/cadastrar.html', context={'colaboradores':colaboradores}) 
+    return render(request, 'app_emprestimo/pages/listar.html', context={'colaboradores':colaboradores})
+
+def atualizar_colaborador(request, idCOLABORADOR):
+    if request.method == 'GET':
+        colaborador = ColaboradorModel.objects.get(idCOLABORADOR=idCOLABORADOR)
+        return render(request, 'app_emprestimo/pages/atualizar_colaborador.html', context={'colaborador':colaborador})
+    
+    nome = request.POST.get('nome')
+    cpf = request.POST.get('cpf')
+    funcao = request.POST.get('funcao')
+    data_admissao = request.POST.get('data_admissao')
+    ColaboradorModel.objects.filter(idCOLABORADOR=idCOLABORADOR).update(nome=nome, cpf=cpf, funcao=funcao, data_admissao=data_admissao)
+    return redirect('listar_colaboradores')
